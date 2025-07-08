d7b6cdd11c8b7063e62778c7788c40c302726aa2
mingdodev <mingdo.dev@gmail.com>
2025-07-08 20:36:17 +0900

    docs: 화면 라우터 구성

--- lib/providers/router_provider.dart
+++ lib/providers/router_provider.dart
@@ -12,25 +12,50 @@ final routerProvider = Provider<GoRouter>((ref) {
     initialLocation: '/login',
     routes: [
       GoRoute(
-        path: '/login',
-        builder: (context, state) => const LoginScreen(),
+        path: '/login', builder: (context, state) => const LoginScreen(),
       ),
       GoRoute(
-        path: '/signup/customer',
-        builder: (context, state) => const SignupCustomerScreen(),
+        path: '/signup/customer', builder: (context, state) => const SignupCustomerScreen(),
       ),
       GoRoute(
-        path: '/signup/merchant',
-        builder: (context, state) => const SignupMerchantScreen(),
+        path: '/signup/merchant', builder: (context, state) => const SignupMerchantScreen(),
       ),
+      
       GoRoute(
-        path: '/home/customer',
-        builder: (context, state) => const HomeCustomerScreen(),
+        path: '/home/customer', builder: (context, state) => const HomeCustomerScreen(),
       ),
+      // GoRoute(
+      //   path: '/home/customer/marketList', builder: (context, state) => const CustomerMarketListScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/customer/marketList/detail', builder: (context, state) => const CustomerMarketDetailScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/customer/marketList/order', builder: (context, state) => const CustomerOrderScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/customer/orderList', builder: (context, state) => const CustomerOrderListScreen(),
+      // ),
+
       GoRoute(
-        path: '/home/merchant',
-        builder: (context, state) => const HomeMerchantScreen(),
+        path: '/home/merchant', builder: (context, state) => const HomeMerchantScreen(),
       ),
+      // GoRoute(
+      //   path: '/home/merchant/productList', builder: (context, state) => const MerchantProductListScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/merchant/productList/add', builder: (context, state) => const MerchantAddProductScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/merchant/orderList', builder: (context, state) => const MerchantOrderListScreen(),
+      // ),
+      // GoRoute(
+      //   path: '/home/merchant/orderList/update', builder: (context, state) => const MerchantUpdateOrderScreen(),
+      // ),
+      
+      // GoRoute(
+      //   path: '/home/chat', builder: (context, state) => const ChatScreen(),
+      // ),
     ],
     redirect: (context, state) {
       final auth = ref.read(authProvider);
