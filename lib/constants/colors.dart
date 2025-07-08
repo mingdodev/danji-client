d58d696b7a962c6ce1e74c312c5ef3367502c8f3
mingdodev <mingdo.dev@gmail.com>
2025-07-08 16:57:54 +0900

    feat: 앱 전체 테마 설정
    - 글꼴 및 색상 설정

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1E88E5);
+  static const secondary = Color(0xFF64B5F6);
+  static const grayDark = Color(0xFF424242);
+  static const grayLight = Color(0xFF79747E);
+  static const background = Color(0xFFF7F7F7);
+}
